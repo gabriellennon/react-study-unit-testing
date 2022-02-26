b407d7f75fb5c82ab1694113f10f06c8f6ff310a
frontend <gabriellennon@wizsolucoes.com.br>
2022-02-25 22:00:24 -0300

    first commit

---
+++ src/App.spec.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('sum', () => {
+    const { getByText } = render(<App />)
+
+    expect(getByText('Hello World')).toBeTruthy()
+})
