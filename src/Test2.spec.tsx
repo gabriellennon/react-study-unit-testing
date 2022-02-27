742c264c7b10fb67219589fbd22a31880110c433
frontend <gabriellennon@wizsolucoes.com.br>
2022-02-27 19:51:41 -0300

    first test

--- src/Test2.spec.tsx
+++ src/Test2.spec.tsx
@@ -1,6 +1,27 @@
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import {Test2} from './Test2';
 
-test('', () => {
+describe('Test2 Component', () => {
+    it('should render list items', () => {
+        const { getByText } = render(<Test2 />)
+
+        //I expect that the the names in screen
+        expect(getByText('Diego')).toBeInTheDocument();
+        expect(getByText('Gabriel')).toBeInTheDocument();
+        expect(getByText('Mayk')).toBeInTheDocument();
+    });
+
+    it('should be able to add new item to the list', () => {
+        const { getByText, debug } = render(<Test2 />)
+
+        //Fire Event permite disparar ações dentro da minha interface da aplicação
+        const addButton = getByText('Adicionar');
+        // debug()
+
+        userEvent.click(addButton);
+        expect(getByText('New name')).toBeInTheDocument();
+        // debug()
+    });
 
 })
--- src/Test2.tsx
+++ src/Test2.tsx
@@ -1,7 +1,18 @@
+import React, { useState } from 'react';
+
 export function Test2(){
+    const [list, setList] = useState(['Diego', 'Gabriel', 'Mayk']);
+
+    function addToList() {
+        setList(state => [...state, 'New name'])
+    }
+
     return(
         <div>
-            
+            <button onClick={addToList}>Adicionar</button>
+            <ul>
+                {list.map(item => <li key={item}>{item}</li>)}
+            </ul>
         </div>
     )
 }
